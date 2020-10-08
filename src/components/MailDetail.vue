c68307170452352e8b308f40ac0f5be3c9810244
Galiov <galilea.valle@gmail.com>
2020-10-08 04:02:42 -0500

    Working on email detail template

---
+++ src/components/MailDetail.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="mail-detail">
+    <div class="grid-item">
+      <p class="from">{{ content.from }}</p>
+    </div>
+    <div class="row align-center tags">
+      Tags
+      <p class="tag">{{ content.tag }}</p>
+    </div>
+    <div class="mail-body">
+      <p>{{ content.body }}</p>
+      <div class="row align-center justify-space-between mail-fotter">
+        <AttachIcon />
+        <button>Replay</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AttachIcon from "../assets/attach_file.svg";
+export default {
+  name: "MailDetail",
+  components: {
+    AttachIcon
+  },
+  props: {
+    content: {
+      type: Object,
+      required: false,
+      default: () => {}
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import "../assets/styles/MailDetail.scss";
+</style>
--- src/components/MailViewer.vue
+++ src/components/MailViewer.vue
@@ -1,10 +1,23 @@
 <template>
-  <div id="mail-viewer"></div>
+  <div id="mail-viewer">
+    <div class="row mail-nav-bar justify-space-between">
+      <div class="row left-btns">
+        <button class="delete-btn">Delete</button>
+        <button class="spam-btn">Spam</button>
+      </div>
+      <button class="unread-btn">Mark as unread</button>
+    </div>
+    <MailDetail :content="$mailData[0]" />
+  </div>
 </template>
 
 <script>
+import MailDetail from "./MailDetail.vue";
 export default {
   name: "MailViewer",
+  components: {
+    MailDetail
+  },
   data() {
     return {};
   }
