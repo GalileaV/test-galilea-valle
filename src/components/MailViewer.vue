f432068d39cf44f1e4606af4cc96389ee0f28c66
Galiov <galilea.valle@gmail.com>
2020-10-08 00:49:24 -0500

    Mail viewer template

---
+++ src/components/MailViewer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="mail-viewer"></div>
+</template>
+
+<script>
+export default {
+  name: "MailViewer",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import "../assets/styles/MailViewer.scss";
+</style>
